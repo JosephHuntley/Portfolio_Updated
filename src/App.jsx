693179152d166791eadb72b6f4fe54a90c9259a6
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-03-03 17:07:51 -0500

    Implement Lazy Loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
-import { useRef } from 'react';
+import { useRef, lazy } from 'react';
 import Header from './components/header/Header';
 import Theme from './styles/theme';
-import BgAnimation from './components/BackgroundAnimation/BackgroundAnimation';
+const BgAnimation = lazy(() =>
+	import('./components/BackgroundAnimation/BackgroundAnimation')
+);
 import { Section } from './styles/globalComponents';
 import Hero from './components/hero/Hero';
-import Projects from './components/projects/Projects';
-import Technologies from './components/technologies/Technologies';
-import Timeline from './components/timeline/TimeLine';
-import Accomplishments from './components/accomplishments/Accomplishments';
+const Projects = lazy(() => import('./components/projects/Projects'));
+const Technologies = lazy(() =>
+	import('./components/technologies/Technologies')
+);
+const Timeline = lazy(() => import('./components/timeline/TimeLine'));
+const Accomplishments = lazy(() =>
+	import('./components/accomplishments/Accomplishments')
+);
 import Footer from './components/footer/Footer';
 import Contact from './components/contact/Contact';
 
