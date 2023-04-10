595bdb803e01ac58afb3d2833489ba0dab64ad1e
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-09 22:46:00 -0400

    Changed design on contact form

--- src/components/contact/ContactStyles.js
+++ src/components/contact/ContactStyles.js
@@ -4,23 +4,36 @@ import Button from '../../styles/globalComponents/Button';
 export const ContactForm = styled.form`
 	display: flex;
 	flex-direction: column;
-	gap: 0.8rem;
+	gap: 2rem;
 `;
 
 export const Input = styled.input`
 	width: 100%;
 	padding: 1.5rem;
 	border-radius: 0.5rem;
+	border-bottom-left-radius:0;
+	border-bottom-right-radius:0;
 	background: transparent;
-	border: 2px solid ${(props) => props.theme.colors.primary1};
-	color: ${(props) => props.theme.colors.primary1};
+	border: none;
+	outline: none;
 	resize: none;
+	border-bottom: 2px solid rgba(242, 245, 247, 0.3);
+	color: ${(props) => props.theme.colors.primary1};
+
+	&:focus{
+		border-bottom: 2px solid rgba(242, 245, 247, 1);
+	}
+	
 `;
 export const TextArea = styled.textarea`
 	width: 100%;
 	padding: 1.5rem;
 	border-radius: 0.5rem;
 	background: transparent;
-	border: 2px solid ${(props) => props.theme.colors.primary1};
+	border: 2px solid rgba(242, 245, 247, 0.3);
 	color: ${(props) => props.theme.colors.primary1};
+
+	&:focus{
+		border-bottom: 2px solid rgba(242, 245, 247, 1);
+	}
 `;
