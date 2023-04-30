624e804e7d7b636fd3f7a1f2cdf9e0b3e4e3397d
Joseph Huntley <Joseph.Huntley@outlook.com>
2023-04-30 15:04:06 -0400

    Added BG3

--- src/components/accomplishments/AccomplishmentsStyles.js
+++ src/components/accomplishments/AccomplishmentsStyles.js
@@ -23,7 +23,7 @@ export const Boxes = styled.div`
 `;
 
 export const Box = styled.div`
-  background: ${(props) => props.theme.colors.background2};
+  background: ${(props) => props.theme.colors.background3};
   border-radius: 12px;
   height: 144px;
   padding: 24px;
--- src/themes/default.js
+++ src/themes/default.js
@@ -8,13 +8,14 @@ export default {
   colors: {
     primary1: "hsl(204,23.8%,95.9%)",
     background1: "#0F1624",
-    background2: "212d45",
+    background3: "#212d45",
     accent1: "hsl(34.9,98.6%,72.9%)",
     button: "hsl(205.1,100%,36.1%)",
     background2: "hsl(232.7,27.3%,23.7%)",
     text: "rgba(255, 255, 255, 0.9)",
     title:
       "linear-gradient(121.57deg,#ffffff 18.77%,rgba(255, 255, 255, 0.66) 60.15%)",
+    accent2: "#5ccfe6",
   },
   // Breakpoints for responsive design
   breakpoints: {
